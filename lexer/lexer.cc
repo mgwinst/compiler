5cf20bfb93054af2bd96c294c7d0a7c9ce444354
mgwinst <matteospatola@hotmail.com>
2025-06-03 23:40:48 -0400

    lots of tables...

--- lexer/lexer.cc
+++ lexer/lexer.cc
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <unordered_set>
+#include <unordered_map>
 #include <cctype>
 #include <fstream>
 #include <iostream>
@@ -15,213 +16,147 @@ namespace {
         "char", "string", "struct", "bool", "void"
     };
 
-    const std::unordered_set<char> skippable = {' ', '\n', '\t', '\r'};
+    const std::unordered_map<State, Token> state_to_token = {
+        {}
+    }
+
+    Category char_to_category[256];
+
+    State transition_table[NUM_STATES][NUM_CHAR_CATEGORIES];
+}
+
+void init_char_categories() {
+    for (int i = 0; i < 256; i++) {
+        char_to_category[i] = Category::OTHER;
+    }
+    
+    for (char c = '0'; c <= '9'; c++) {
+        char_to_category[(int)c] = Category::DIGIT;
+    }
+
+    for (char c = 'A'; c <= 'Z'; c++) {
+        char_to_category[(int)c] = Category::LETTER;
+    }
+
+    for (char c = 'a'; c <= 'z'; c++) {
+        char_to_category[(int)c] = Category::LETTER;
+    }
+
+    char_to_category[(int)'='] = Category::EQUAL;
+    char_to_category[(int)'+'] = Category::PLUS;
+    char_to_category[(int)'-'] = Category::MINUS;
+    char_to_category[(int)'*'] = Category::STAR;
+    char_to_category[(int)'/'] = Category::SLASH;
+    char_to_category[(int)'<'] = Category::LESS_THAN;
+    char_to_category[(int)'>'] = Category::GREATER_THAN;
+    char_to_category[(int)'('] = Category::LPAREN;
+    char_to_category[(int)')'] = Category::RPAREN;
+    char_to_category[(int)'{'] = Category::LBRACKET;
+    char_to_category[(int)'}'] = Category::RBRACKET;
+    char_to_category[(int)'['] = Category::LBRACE;
+    char_to_category[(int)']'] = Category::RBRACE;
+    char_to_category[(int)'&'] = Category::AMPERSAND,
+    char_to_category[(int)'"'] = Category::DOUBLE_QUOTE;
+    char_to_category[(int)';'] = Category::SEMICOLON;
+    char_to_category[(int)':'] = Category::COLON;
+    char_to_category[(int)','] = Category::COMMA;
+    char_to_category[(int)'.'] = Category::DOT;
+    char_to_category[(int)'!'] = Category::EXCLA_POINT;
+    char_to_category[(int)'?'] = Category::QUESTION;
+    char_to_category[(int)'^'] = Category::CARROT;
+    char_to_category[(int)'_'] = Category::UNDERSCORE;
+
+    // all Categoroy::WHITESPACE or unique?
+    char_to_category[(int)' '] = Category::WHITESPACE;
+    char_to_category[(int)'\n'] = Category::WHITESPACE;
+    char_to_category[(int)'\r'] = Category::WHITESPACE;
+    char_to_category[(int)'\t'] = Category::WHITESPACE;
+}
+
+void init_transition_table() {
+    for (int i = 0; i < 64; i++) {
+        for (int j = 0; j < 64; j++) {
+            transition_table[i][j] = State::INVALID;
+        }
+    }
+
+    transition_table[(int)State::INITIAL][(int)Category::LETTER] = State::ID_START; 
+    transition_table[(int)State::ID_START][(int)Category::LETTER] = State::ID_CHAR; 
+    transition_table[(int)State::ID_CHAR][(int)Category::LETTER] = State::ID_CHAR;
+    transition_table[(int)State::ID_CHAR][(int)Category::DIGIT] = State::ID_CHAR;
+    transition_table[(int)State::ID_CHAR][(int)Category::WHITESPACE] = State::ID_ACCEPT;
+
+    transition_table[(int)State::INITIAL][(int)Category::DIGIT] = State::DIGIT_START;
+    transition_table[(int)State::DIGIT_START][(int)Category::DIGIT] = State::DIGIT_CHAR;
+    transition_table[(int)State::DIGIT_CHAR][(int)Category::DIGIT] = State::DIGIT_CHAR;
+    transition_table[(int)State::DIGIT_CHAR][(int)Category::WHITESPACE] = State::DIGIT_ACCEPT;
+
+    transition_table[(int)State::INITIAL][(int)Category::DOUBLE_QUOTE] = State::STRING_LITERAL_START; 
+    transition_table[(int)State::STRING_LITERAL_START][(int)Category::LETTER] = State::STRING_LITERAL_CHAR; 
+    transition_table[(int)State::STRING_LITERAL_START][(int)Category::DIGIT] = State::STRING_LITERAL_CHAR; 
+    transition_table[(int)State::STRING_LITERAL_CHAR][(int)Category::LETTER] = State::STRING_LITERAL_CHAR; 
+    transition_table[(int)State::STRING_LITERAL_CHAR][(int)Category::DIGIT] = State::STRING_LITERAL_CHAR; 
+    transition_table[(int)State::STRING_LITERAL_CHAR][(int)Category::WHITESPACE] = State::STRING_LITERAL_CHAR; 
+    transition_table[(int)State::STRING_LITERAL_CHAR][(int)Category::DOUBLE_QUOTE] = State::STRING_LITERAL_ACCEPT; 
+
+    transition_table[(int)State::INITIAL][(int)Category::EQUAL] = State::ASSIGN_ACCEPT;
+    transition_table[(int)State::INITIAL][(int)Category::PLUS] = State::ADD_ACCEPT;
+    transition_table[(int)State::INITIAL][(int)Category::MINUS] = State::SUB_ACCEPT;
+    transition_table[(int)State::INITIAL][(int)Category::STAR] = State::MULTIPLY_ACCEPT;
+    transition_table[(int)State::INITIAL][(int)Category::SLASH] = State::DIVIDE_ACCEPT;
+    transition_table[(int)State::INITIAL][(int)Category::LESS_THAN] = State::LESS_THAN_ACCEPT;
+    transition_table[(int)State::INITIAL][(int)Category::GREATER_THAN] = State::GREATER_THAN_ACCEPT;
+    transition_table[(int)State::INITIAL][(int)Category::LPAREN] = State::LPAREN_ACCEPT;
+    transition_table[(int)State::INITIAL][(int)Category::RPAREN] = State::RPAREN_ACCEPT;
+    transition_table[(int)State::INITIAL][(int)Category::LBRACKET] = State::LBRACKET_ACCEPT;
+    transition_table[(int)State::INITIAL][(int)Category::RBRACKET] = State::RBRACKET_ACCEPT;
+    transition_table[(int)State::INITIAL][(int)Category::LBRACE] = State::LBRACE_ACCEPT;
+    transition_table[(int)State::INITIAL][(int)Category::RBRACE] = State::RBRACE_ACCEPT; 
+    transition_table[(int)State::INITIAL][(int)Category::AMPERSAND] = State::ADDRESSOF_ACCEPT; 
+    transition_table[(int)State::INITIAL][(int)Category::SEMICOLON] = State::SEMICOLON_ACCEPT;
+    transition_table[(int)State::INITIAL][(int)Category::COLON] = State::COLON_ACCEPT; 
+    transition_table[(int)State::INITIAL][(int)Category::COMMA] = State::COMMA_ACCEPT;
+    transition_table[(int)State::INITIAL][(int)Category::DOT] = State::DOT_ACCEPT; 
+    transition_table[(int)State::INITIAL][(int)Category::EXCLA_POINT] = State::EXCLA_ACCEPT;
+    transition_table[(int)State::INITIAL][(int)Category::QUESTION] = State::QUESTION_ACCEPT;
+    transition_table[(int)State::INITIAL][(int)Category::CARROT] = State::CARROT_ACCEPT; 
+    transition_table[(int)State::INITIAL][(int)Category::UNDERSCORE] = State::UNDERSCORE_ACCEPT;
+    transition_table[(int)State::INITIAL][(int)Category::BAR] = State::BAR_ACCEPT;
+
+    transition_table[(int)State::ASSIGN_ACCEPT][(int)Category::EQUAL] = State::EQUALS_ACCEPT;
+    transition_table[(int)State::ADD_ACCEPT][(int)Category::PLUS] = State::INCREMENT_ACCEPT;
+    transition_table[(int)State::ADD_ACCEPT][(int)Category::EQUAL] = State::ADD_ASSIGN_ACCEPT;
+    transition_table[(int)State::SUB_ACCEPT][(int)Category::MINUS] = State::DECREMENT_ACCEPT;
+    transition_table[(int)State::SUB_ACCEPT][(int)Category::EQUAL] = State::SUB_ASSIGN_ACCEPT;
+    transition_table[(int)State::MULTIPLY_ACCEPT][(int)Category::EQUAL] = State::MULTIPLY_ASSIGN_ACCEPT;
+    transition_table[(int)State::DIVIDE_ACCEPT][(int)Category::EQUAL] = State::DIVIDE_ASSIGN_ACCEPT;
+    transition_table[(int)State::LESS_THAN_ACCEPT][(int)Category::EQUAL] = State::LESS_EQUAL_ACCEPT;
+    transition_table[(int)State::GREATER_THAN_ACCEPT][(int)Category::EQUAL] = State::GREATER_EQUAL_ACCEPT;
+    transition_table[(int)State::ADDRESSOF_ACCEPT][(int)Category::AMPERSAND] = State::LOGICAL_AND_ACCEPT;
+    transition_table[(int)State::EXCLA_ACCEPT][(int)Category::EQUAL] = State::LOGICAL_NOT_ACCEPT;
+    transition_table[(int)State::BAR_ACCEPT][(int)Category::BAR] = State::LOGICAL_OR_ACCEPT;
+    transition_table[(int)State::DIVIDE_ACCEPT][(int)Category::SLASH] = State::COMMENT_ACCEPT;
+    transition_table[(int)State::SUB_ACCEPT][(int)Category::GREATER_THAN] = State::ARROW_ACCEPT;
 }
 
 
 
+std::optional<Token> transition(State& current_state, const char* cur_char) {
+    auto new_state = transition_table[(int)current_state][(int)(*cur_char)];
+    
+}
 
 
-[[nodiscard]] std::vector<Token> lex(std::string_view source) {
-    std::vector<Token> tokens;
+[[nodiscard]] std::vector<Token> lex2(std::string_view source) {
+    std::vector<Token> tokens;   
 
     auto cur = source.begin();
     auto start = cur, end = cur;
 
+    State current_state = State::INITIAL;
+
     while (cur != source.end()) {
-        // whitespace handler
-        if (std::isspace(*cur)) {
-            if (*cur == ' ') {
-                cur++;
-            }
-            
-
-        } else if (std::isdigit(*cur)) {
-            start = cur;
-            while (std::isdigit(*cur)) cur++;
-            end = cur;
-            tokens.emplace_back(TokenType::NUMERIC_LITERAL, std::string{start, end});
-
-        } else if (std::isalpha(*cur)) {
-            start = cur;
-            while (std::isalnum(*cur) || *cur == '_') cur++;
-            end = cur;
-
-            std::string_view value{start, end};
-            TokenType type = TokenType::IDENTIFIER;
-
-            if (value == "if")
-                type = TokenType::KEYWORD_IF;
-            else if (value == "else")
-                type = TokenType::KEYWORD_ELSE;
-            else if (value == "while")
-                type = TokenType::KEYWORD_WHILE;
-            else if (value == "for")
-                type = TokenType::KEYWORD_FOR;
-            else if (value == "return") 
-                type = TokenType::KEYWORD_RETURN;
-            else if (value == "const")
-                type = TokenType::KEYWORD_CONST;
-            else if (value == "typedef")
-                type = TokenType::KEYWORD_TYPEDEF;
-            else if (type_keywords.contains(value))
-                type = TokenType::TYPE;
-
-            tokens.emplace_back(type, std::string{value});
-        
-        } else {
-            switch (*cur) {
-                case '=':
-                    if (*(cur+1) == '=') {
-                        tokens.emplace_back(TokenType::EQUALS, std::string{cur, cur+2});
-                        cur += 2;
-                    } else {
-                        tokens.emplace_back(TokenType::ASSIGN, std::string{*cur});
-                        cur++;
-                    }
-                    break;
-                case '+':
-                    if (*(cur+1) == '=') {
-                        tokens.emplace_back(TokenType::ADD_ASSIGN, std::string{cur, cur+2});
-                        cur += 2;
-                    } else if (*(cur+1) == '+') {
-                        tokens.emplace_back(TokenType::INCREMENT, std::string{cur, cur+2});
-                        cur += 2;
-                    } else {
-                        tokens.emplace_back(TokenType::ADD, std::string{*cur});
-                        cur++;
-                    }
-                    break;
-                case '-':
-                    if (*(cur+1) == '=') {
-                        tokens.emplace_back(TokenType::SUBTRACT_ASSIGN, std::string{cur, cur+2});
-                        cur += 2;
-                    }else if (*(cur+1) == '-') {
-                        tokens.emplace_back(TokenType::DECREMENT, std::string{cur, cur+2});
-                        cur += 2;
-                    } else if (*(cur+1) == '>') {
-                        tokens.emplace_back(TokenType::ARROW, std::string{cur, cur+2});
-                        cur += 2;
-                    } else {
-                        tokens.emplace_back(TokenType::SUBTRACT, std::string{*cur});
-                        cur++;
-                    }
-                    break;
-                case '*':
-                    if (*(cur+1) == '=') {
-                        tokens.emplace_back(TokenType::MULTIPLY_ASSIGN, std::string{cur, cur+2});
-                        cur += 2;
-                    } else {
-                        tokens.emplace_back(TokenType::MULTIPLY, std::string{*cur});
-                        cur++;
-                    }
-                    break;
-                case '/':
-                    if (*(cur+1) == '=') {
-                        tokens.emplace_back(TokenType::DIVIDE_ASSIGN, std::string{cur, cur+2});
-                        cur += 2; 
-                    } else if (*(cur+1) == '/') {
-                        tokens.emplace_back(TokenType::COMMENT, std::string{cur, cur+2});
-                        cur += 2;
-                    } else {
-                        tokens.emplace_back(TokenType::DIVIDE, std::string{*cur});
-                        cur++;
-                    }
-                    break;
-                case '%':
-                    tokens.emplace_back(TokenType::MODULUS, std::string{*cur});
-                    cur++;
-                    break;
-                case '(':
-                    tokens.emplace_back(TokenType::LPAREN, std::string{*cur});
-                    cur++;
-                    break;
-                case ')':
-                    tokens.emplace_back(TokenType::RPAREN, std::string{*cur});
-                    cur++;
-                    break;
-                case '{':
-                    tokens.emplace_back(TokenType::LBRACE, std::string{*cur});
-                    cur++;
-                    break;
-                case '}':
-                    tokens.emplace_back(TokenType::RBRACE, std::string{*cur});
-                    cur++;
-                    break;
-                case '[':
-                    tokens.emplace_back(TokenType::LBRACKET, std::string{*cur});
-                    cur++;
-                    break;
-                case ']':
-                    tokens.emplace_back(TokenType::RBRACKET, std::string{*cur});
-                    cur++;
-                    break;
-                case '>':
-                    if (*(cur+1) == '=') {
-                        tokens.emplace_back(TokenType::GREATER_EQUAL, std::string{cur, cur+2});
-                        cur += 2;
-                    } else {
-                        tokens.emplace_back(TokenType::GREATER_THAN, std::string{*cur});
-                        cur++;
-                    }
-                    break;
-                case '<':
-                    if (*(cur+1) == '=') {
-                        tokens.emplace_back(TokenType::LESS_EQUAL, std::string{cur, cur+2});
-                        cur += 2;
-                    } else {
-                        tokens.emplace_back(TokenType::LESS_THAN, std::string{*cur});
-                        cur++;
-                    }
-                    break;
-                case '&':
-                    if (*(cur+1) == '&') {
-                        tokens.emplace_back(TokenType::LOGICAL_AND, std::string{cur, cur+2});
-                        cur += 2;
-                    } else {
-                        tokens.emplace_back(TokenType::ADDRESS_OF, std::string{*cur});
-                        cur++;
-                    }
-                    break;
-                case '"':
-                    start = cur++;
-                    while (*cur != '"') cur++;
-                    end = cur;
-                    tokens.emplace_back(TokenType::STRING_LITERAL, std::string{start, end+1});
-                    cur++;
-                    break;
-                case ';':
-                    tokens.emplace_back(TokenType::SEMICOLON, std::string{*cur});
-                    cur++;
-                    break;
-                case ':':
-                    tokens.emplace_back(TokenType::COLON, std::string{*cur});
-                    cur++;
-                    break;
-                case ',':
-                    tokens.emplace_back(TokenType::COMMA, std::string{*cur});
-                    cur++;
-                    break;
-                case '.':
-                    tokens.emplace_back(TokenType::DOT, std::string{*cur});
-                    cur++;
-                    break;
-                case '!':
-                    if (*(cur+1) == '=') {
-                        tokens.emplace_back(TokenType::NOT_EQUAL, std::string{cur, cur+2});
-                        cur += 2;
-                    } else {
-                        tokens.emplace_back(TokenType::LOGICAL_NOT, std::string{*cur});
-                        cur++;
-                    }
-                    break;
-                case EOF:
-                    tokens.emplace_back(TokenType::END_OF_FILE, std::string{*cur});
-                    break;
-                default:
-                    throw std::runtime_error("Invalid token: " + std::string{*cur});
-            }
-        }
+        transition(current_state, cur);
     }
 
     return tokens;
@@ -249,6 +184,7 @@ void test_lex(std::string path) {
 
 int main() {
     // test_lex("../test/sample_program.c");
-    test_lex("../test/sample_tokens.txt");
+    // test_lex("../test/sample_tokens.txt");
+
 
 }
--- lexer/lexer.h
+++ lexer/lexer.h
@@ -1,5 +1,10 @@
 #include <cstdint>
 #include <string>
+#include <optional>
+#include <vector>
+
+#define NUM_STATES 64
+#define NUM_CHAR_CATEGORIES 64
 
 enum class TokenType : uint16_t {
     IDENTIFIER = 128,
@@ -61,24 +66,130 @@ enum class TokenType : uint16_t {
 
     COMMENT,
     END_OF_FILE,
-    INVALID,   
+    INVALID,
 
     // handle bitwise later
 };
 
+enum class State : uint16_t {
+    INITIAL,
+
+    ID_START,
+    ID_CHAR,
+    ID_ACCEPT,
+
+    KEYWORD_ACCEPT,
+
+    DIGIT_START,
+    DIGIT_CHAR,
+    DIGIT_ACCEPT,
+
+    STRING_LITERAL_START,
+    STRING_LITERAL_CHAR,
+    STRING_LITERAL_ACCEPT,
+
+    ASSIGN_ACCEPT,
+    EQUALS_ACCEPT,
+
+    ADD_ACCEPT,
+    ADD_ASSIGN_ACCEPT,
+    SUB_ACCEPT,
+    SUB_ASSIGN_ACCEPT,
+    INCREMENT_ACCEPT,
+    DECREMENT_ACCEPT,
+    MULTIPLY_ACCEPT,
+    MULTIPLY_ASSIGN_ACCEPT,
+    DIVIDE_ACCEPT,
+    DIVIDE_ASSIGN_ACCEPT,
+
+    LESS_THAN_ACCEPT,
+    LESS_EQUAL_ACCEPT,
+    GREATER_THAN_ACCEPT,
+    GREATER_EQUAL_ACCEPT,
+
+    LOGICAL_AND_ACCEPT,
+    LOGICAL_OR_ACCEPT,
+    LOGICAL_NOT_ACCEPT,
+
+    LPAREN_ACCEPT,
+    RPAREN_ACCEPT,
+    LBRACKET_ACCEPT,
+    RBRACKET_ACCEPT,
+    LBRACE_ACCEPT,
+    RBRACE_ACCEPT,
+    SEMICOLON_ACCEPT,
+    COLON_ACCEPT,
+    COMMA_ACCEPT,
+    DOT_ACCEPT,
+    EXCLA_ACCEPT,
+    QUESTION_ACCEPT,
+    CARROT_ACCEPT,
+    UNDERSCORE_ACCEPT,
+    BAR_ACCEPT,
+    COMMENT_ACCEPT,
+
+    ADDRESSOF_ACCEPT,
+    ARROW_ACCEPT,
+
+    INVALID
+};
+
+enum class Category : uint16_t {
+    WHITESPACE,
+    LETTER,
+    DIGIT,
+    EQUAL,
+    PLUS,
+    MINUS,
+    STAR,
+    SLASH,
+    LESS_THAN,
+    GREATER_THAN,
+    LPAREN,
+    RPAREN,
+    LBRACE,
+    RBRACE,
+    LBRACKET,
+    RBRACKET,
+    AMPERSAND,
+    SINGLE_QUOTE,
+    DOUBLE_QUOTE,
+    SEMICOLON,
+    COLON,
+    COMMA,
+    DOT,
+    CARROT,
+    UNDERSCORE,
+    EXCLA_POINT,
+    QUESTION,
+    BAR,
+    OTHER
+};
+
 struct Token {
     TokenType type;
-    std::string lexeme;
-    int line_number;
-    int column_number;
-    int length;
-    
+    std::optional<std::string_view> lexeme;
+    std::size_t line_number;
+    std::size_t column_number;
+    std::size_t length;
+
+    Token(TokenType t, std::string_view l, std::size_t ln, std::size_t cn, std::size_t len) :
+        type{t}, lexeme{l}, line_number{ln}, column_number{cn}, length{len} {}
+
+    ~Token() {}
+
+    Token(const Token&) = delete;
+    Token& operator=(const Token&) = delete;
+    Token(Token&&) = delete;
+    Token& operator=(Token&&) = delete;
 
     std::string to_string() const {
-        return std::to_string(static_cast<int>(type)) + " " + lexeme;
+        return std::to_string(static_cast<int>(type)) + " " + std::string{lexeme.value()};
     }
 };
 
 [[nodiscard]] std::vector<Token> lex(std::string_view);
+
+std::optional<Token> transition(State&, const char);
 
 void test_lex(std::string);
